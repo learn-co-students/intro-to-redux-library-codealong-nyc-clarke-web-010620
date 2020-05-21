2e535c1f5ad384e0a85feca4d0e24286afa59b71
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-05-21 13:45:35 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,47 @@ import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
+	
 	handleOnClick = event => {
 		this.props.increaseCount();
 	};
+
+
+	testShow(x) {
+		if (x < 4) {
+			return ( <div><p>0</p></div> )
+		} else {
+			let ans = 0
+			let i 
+			for (i = 0; i < x; i++) {
+				if (i % 3 === 0 || i % 5 === 0) {
+					ans += i; 
+				}
+			}
+
+			return (
+				<div>
+					<p>Oh {ans}</p>
+				</div>
+			)
+		}
+	}
+
 
 	render() {
 		return (
 			<div className="App">
 				<button onClick={this.handleOnClick}>Click</button>
 				<p>{this.props.items.length}</p>
+				<br /><br />
+				{this.testShow(1000)}
 			</div>
 		);
 	}
 }
+
+
+
 
 const mapStateToProps = state => {
 	return {
